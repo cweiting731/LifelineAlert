5344f708f1dddeac59dfce4e0c81a45818cba82c
Nues0913 <106960429+Nues0913@users.noreply.github.com>
2025-02-14 20:53:59 +0800

    add Data class for UIs in the viewModel(MVVM)

---
+++ app/src/main/java/com/example/lifelinealert/page/mapViewModel/MapUiState.kt
@@ -0,0 +1,10 @@
+package com.example.lifelinealert.page.mapViewModel
+
+import com.google.android.gms.maps.model.LatLng
+
+data class MapUiState(
+    val nckuLibrary: LatLng = LatLng(22.999973101427155, 120.21985214463398),
+    val locations: Map<String, LatLng> = emptyMap(),
+    val polylinePaths: Map<String, List<LatLng>> = emptyMap()
+
+)
