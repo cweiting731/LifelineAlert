1ab9a41c5b153507578acc5a1964baea29fed888
Nues0913 <106960429+Nues0913@users.noreply.github.com>
2025-02-15 14:11:59 +0800

    優化線程的切換代碼

--- app/src/main/java/com/example/lifelinealert/page/mapViewModel/MapViewModel.kt
+++ app/src/main/java/com/example/lifelinealert/page/mapViewModel/MapViewModel.kt
@@ -1,14 +1,18 @@
 package com.example.lifelinealert.page.mapViewModel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.lifelinealert.utils.map.Directions.fetchRoute
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MapViewModel : ViewModel() {
 
@@ -25,33 +29,31 @@ class MapViewModel : ViewModel() {
 
     // 救護車測試資料模擬
     private fun startTargetFetchingSimulating() {
-        viewModelScope.launch {
-            while (true) {
+        viewModelScope.launch(Dispatchers.IO) {
+            while (isActive) {
                 val updatedLocations = _uiState.value.locations.toMutableMap().apply {
                     put("A", LatLng(22.999973101427155 + Math.random() * 0.001, 120.2198 + Math.random() * 0.001))
                     put("B", LatLng(22.999973101427155 + Math.random() * 0.001, 120.2208 + Math.random() * 0.001))
                     put("C", LatLng(22.999973101427155 + Math.random() * 0.001, 120.2218 + Math.random() * 0.001))
                 }
-                // update state
-                _uiState.value = _uiState.value.copy(locations = updatedLocations)
-                fetchPathsForTargets(updatedLocations)
-
+//                Log.d("ThreadInfo", "目前執行緒: ${Thread.currentThread().name}")
+                withContext(Dispatchers.Main) {
+                    // update state
+                    _uiState.value = _uiState.value.copy(locations = updatedLocations)
+                    fetchPathsForTargets(updatedLocations)
+                }
                 delay(2000) // 每 2 秒更新一次位置
             }
         }
     }
 
     private fun fetchPathsForTargets(targetLocations: Map<String, LatLng>) {
-        targetLocations.forEach { (id, location) ->
-            viewModelScope.launch {
-                val path = fetchRoute(location, nckuLibrary)
-
-                // update state
-                val updatedPaths = _uiState.value.polylinePaths.toMutableMap().apply {
-                    put(id, path)
-                }
-                _uiState.value = _uiState.value.copy(polylinePaths = updatedPaths)
+        viewModelScope.launch {
+            val updatedPaths = targetLocations.mapValues { (_, location) ->
+                fetchRoute(location, nckuLibrary)
             }
+
+            _uiState.value = _uiState.value.copy(polylinePaths = updatedPaths)
         }
     }
 }
