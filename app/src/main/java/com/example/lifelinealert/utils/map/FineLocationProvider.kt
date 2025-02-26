356b2828d43d55ec63c59259ca34ae3e96a0b0d6
Nues0913 <a09871530274@gmail.com>
2025-02-26 11:31:56 +0800

    優化位置請求，多載request方法

--- app/src/main/java/com/example/lifelinealert/utils/map/FineLocationProvider.kt
+++ app/src/main/java/com/example/lifelinealert/utils/map/FineLocationProvider.kt
@@ -12,20 +12,35 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 
 object FineLocationProvider {
-    val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 200)
+
+    /**
+     * LocationRequest is configured to request location updates with high accuracy and a small update interval.
+     * - Priority.PRIORITY_HIGH_ACCURACY: Requests the most accurate location.
+     * - Update interval of 200 ms, with a minimum update interval of 100 ms.
+     */
+    private val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 200)
         .setMinUpdateIntervalMillis(100)
         .build()
-    val locationCallback = object : LocationCallback() {
+    private val defaultLocationCallback: LocationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             // code
             locationResult ?: return
         }
     }
+    private var locationCallback: LocationCallback = defaultLocationCallback
     var isLocationRequesting = false
         private set
     private var fusedLocationProvider: FusedLocationProviderClient? = null
 
-
+    /**
+     * Requests location updates with the default location callback.
+     * You can only request once in the global.
+     * This callback will only request for location update,
+     * call another overloading function for custom your callback`
+     * Ensures that required permissions (ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION) are granted.
+     * @param activity The activity context used to get the FusedLocationProviderClient.
+     * @throws Exception if location updates are already requested or if permissions are missing.
+     */
     fun requestLocationUpdates(activity: Activity) {
         if(fusedLocationProvider != null) {
             throw Exception("Location updates has already requested")
@@ -45,6 +60,40 @@ object FineLocationProvider {
         isLocationRequesting = true
     }
 
+    /**
+     * Requests location updates with a custom location callback.
+     * You can only request once in the global.
+     * Ensures that required permissions (ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION) are granted.
+     * @param activity The activity context used to get the FusedLocationProviderClient.
+     * @param locationCallback A custom callback to handle location updates.
+     * @throws Exception if location updates are already requested or if permissions are missing.
+     */
+    fun requestLocationUpdates(activity: Activity, locationCallback: LocationCallback) {
+        if(fusedLocationProvider != null) {
+            throw Exception("Location updates has already requested")
+        }
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(activity)
+        if (ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            throw Exception("ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION permissions missing")
+        }
+        this.locationCallback = locationCallback
+        fusedLocationProvider!!.requestLocationUpdates(locationRequest, locationCallback, null)
+        isLocationRequesting = true
+    }
+
+    /**
+     * Removes location updates.
+     * Resets the location callback to the default callback.
+     * If location updates were not requested previously, an exception will be thrown.
+     * @throws Exception if no location updates need to be removed.
+     */
     fun removeLocationUpdates() {
         if(fusedLocationProvider == null) {
             throw Exception("No updates need to remove")
@@ -52,5 +101,6 @@ object FineLocationProvider {
         fusedLocationProvider?.removeLocationUpdates(locationCallback)
         fusedLocationProvider = null
         isLocationRequesting = false
+        locationCallback = defaultLocationCallback
     }
 }
