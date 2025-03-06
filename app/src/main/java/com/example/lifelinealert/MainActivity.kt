a4881f6a0d8f27df61d52c6d60116b3d05cd6222
willy Chou <willy931118@gmail.com>
2025-03-06 10:20:23 +0800

    split MainActivity's UI to MainPage

--- app/src/main/java/com/example/lifelinealert/MainActivity.kt
+++ app/src/main/java/com/example/lifelinealert/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -50,11 +49,6 @@ import com.example.lifelinealert.utils.manager.SnackbarManager
 
 class MainActivity : ComponentActivity() {
     private lateinit var viewModel: MainActivityViewModel
-    private var showDialog = mutableStateOf(false)
-    private var alertDialogTitle = mutableStateOf("警告")
-    private var alertDialogMessage = mutableStateOf("OnPause")
-    private val permissionRequestCode = 10
-    private val left_channelId = "left_emergency"
     private val right_channelId = "right_emergency"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -159,92 +153,5 @@ class MainActivity : ComponentActivity() {
         else {
             viewModel.setShowController(true)
         }
-    }
-}
-
-@Composable
-fun MainPage(viewModel: MainActivityViewModel) {
-    val navController = rememberNavController()
-    val showController by viewModel.showController
-
-    Scaffold(
-        snackbarHost = { SnackbarHost(hostState = SnackbarManager.snackbarHostState) },
-    ) { innerPadding ->
-        NavHostContainer(navController,
-            Modifier
-                .padding(innerPadding)
-                .fillMaxSize())
-
-        if (showController) {
-            Column {
-                Spacer(modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f, true))
-                BottomNavigationBar(navController = navController, viewModel = viewModel)
-            }
-        }
-    }
-}
-
-sealed class Page(
-    val route: String,
-    val title: Int,
-    val icon: Int
-) {
-    object Map : Page("map", R.string.page_map_title, R.drawable.ic_navigation_bar_map)
-    object Profile :
-        Page("profile", R.string.page_profile_title, R.drawable.ic_navigation_bar_profile)
-
-    object Point : Page("point", R.string.page_point_title, R.drawable.ic_navigation_bar_point)
-}
-
-//@Preview(showBackground = true)
-@Composable
-fun BottomNavigationBar(navController: NavHostController, viewModel: MainActivityViewModel) {
-    val pages = listOf(
-        Page.Map,
-        Page.Profile,
-        Page.Point
-    )
-    Row (
-        modifier = Modifier
-            .padding(10.dp, 10.dp, 10.dp, 30.dp)
-            .clip(RoundedCornerShape(20.dp))
-            .background(color = Color(0xCCF2EDF7)),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        val currentRoute by viewModel.pageRoute
-        val selectedColor = Color(0xFF6650a4)
-        val unselectedColor = Color(0xFF000000)
-
-        pages.forEach { page ->
-            NavigationBarItem(
-                onClick = {
-                    if (page.route != currentRoute) {
-                        navController.navigate(page.route)
-                        viewModel.setPageRoute(page.route)
-                    }
-                },
-                selected = (currentRoute == page.route),
-                label = { Text(text = stringResource(id = page.title)) },
-                icon = {
-                    Icon(
-                        painter = painterResource(id = page.icon),
-                        contentDescription = stringResource(id = page.title),
-                        tint = if (currentRoute == page.route) selectedColor else unselectedColor
-                    )
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun NavHostContainer(navController: NavHostController, modifier: Modifier = Modifier) {
-    NavHost(navController = navController, startDestination = Page.Map.route) {
-        composable(Page.Map.route) { MapPage() }
-        composable(Page.Profile.route) { ProfilePage() }
-        composable(Page.Point.route) { PointPage() }
     }
 }
